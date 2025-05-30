c1e7663d8e424c95d57ad43b41250d9e23ed7c95
aswarb <andrewoswarbrick@hotmail.co.uk>
2025-05-30 19:39:51 +0100

    Started work on TaskEngine.go
    On reflection the task engine will need a map of all *exec.Cmd created.
    In this way the execution logic and pipes are separate from the
    structural data queries through the resolver

--- internal/task/TaskEngine.go
+++ internal/task/TaskEngine.go
@@ -1,11 +1,47 @@
 package task
 
-import ()
+import (
+	"context"
+	wp "orchest-client/internal/workerpool"
+	"os/exec"
+)
 
 type TaskEngine struct {
 	resolver *DAGResolver
+	cmdMap   map[string]*exec.Cmd
+}
+type ParallelTaskArgs struct {
+	startUid   string
+	currentUid string
+}
+
+func (p ParallelTaskArgs) IsTask() bool {
+
+	return true
+
 }
 
 // Creates pipes for adjacent nodes
-func (t *TaskEngine) createPipes(tasks []Task) {
+func (t *TaskEngine) createPipes() {
 }
+
+func (t *TaskEngine) executeParallelTask(segmentUid string) {
+	ctx, cancelFunc := context.WithCancel(context.Background())
+
+	segment, _ := t.resolver.GetSegment(segmentUid)
+
+	workerpool := wp.MakeWorkerPool(ctx)
+	workerpool.AddWorkers(uint(1))
+	startedTasks := make(map[string]struct{})
+
+	cancelFunc()
+}
+func (t *TaskEngine) onParallelExecute(w *wp.Worker, wt *wp.WorkerTask) error {
+	args := wt.Args.(*ParallelTaskArgs)
+	node, _ := t.resolver.GetNode(args.startUid)
+	args.currentUid = node.GetUid()
+
+	return nil
+}
+func (t *TaskEngine) onParallelComplete(w *wp.Worker, wt *wp.WorkerTask) {}
+func (t *TaskEngine) onParallelError(w *wp.Worker, wt *wp.WorkerTask)    {}
--- internal/task/TaskManager.go
+++ internal/task/TaskManager.go
@@ -4,6 +4,22 @@ import (
 // wp "orchest-client/internal/workerpool"
 )
 
+func FilterMap[K comparable, V any](m map[K]V, isValid func(K, V) bool) map[K]V {
+	result := make(map[K]V)
+	for k, v := range m {
+		if isValid(k, v) {
+			result[k] = v
+		}
+	}
+	return result
+}
+
+func MapHasKey[K comparable, V any](m map[K]V, key K) bool {
+	_, ok := m[key]
+	return ok
+
+}
+
 type TaskManager struct {
 	engine   *TaskEngine
 	resolver *DAGResolver
