d0b357627d9ba0b49cb6675c5f71eeb4f87948aa
aswarb <andrewoswarbrick@hotmail.co.uk>
2025-06-28 01:40:16 +0100

    [WIP] Refactor: Added buffer consumer

--- internal/task/CmdWrapper.go
+++ internal/task/CmdWrapper.go
@@ -58,10 +58,11 @@ func (p *PipeWrapper) IsOpen() bool {
 }
 
 type ProcessEndpoint struct {
-	cmd    *exec.Cmd
-	in     *OutPipeEndpoint
-	out    *InPipeEndpoint
-	buffer Buffer
+	cmd            *exec.Cmd
+	inPipe         *PipeWrapper
+	outPipe        *PipeWrapper
+	fromBufferPipe *PipeWrapper
+	buffer         Buffer
 }
 
 func (p *ProcessEndpoint) BufferedStdinWrite(data []byte) {
@@ -73,19 +74,51 @@ func (p *ProcessEndpoint) ExecuteBlocking() error {
 	return err
 }
 
-func (p *ProcessEndpoint) SetInPipeEndpoint(endpoint *OutPipeEndpoint) {
-	p.in = endpoint
-	p.cmd.Stdin = endpoint.GetEndpoint()
+func (p *ProcessEndpoint) EnableBuffer() {
+	p.cmd.Stdin = p.fromBufferPipe.GetOutReader()
 }
 
-func (p *ProcessEndpoint) SetOutPipeEndpoint(endpoint *InPipeEndpoint) {
-	p.out = endpoint
-	p.cmd.Stdout = endpoint.GetEndpoint()
+type consumerTaskArgs struct {
+}
+
+func (c *consumerTaskArgs) IsTask() bool
+
+func (p *ProcessEndpoint) startStdinConsumers(ctx context.Context) {
+	consumerFuncExecute := func(w *wp.Worker, wt *wp.WorkerTask) error {
+		for {
+			data := make([]byte, 4096)
+			n, err := p.inPipe.Read(data)
+			data = data[:n]
+			if err == io.EOF {
+				return io.EOF
+			}
+
+			p.buffer.Write(data)
+		}
+	}
+
+	consumerFuncComplete := func(*wp.Worker, *wp.WorkerTask) {}
+	consumerFuncError := func(*wp.Worker, *wp.WorkerTask, error) {}
+
+	workerpool := wp.MakeWorkerPool(ctx)
+	taskArgs := consumerTaskArgs{}
+	workerpool.AddWorkers(uint(1))
+	task := wp.WorkerTask{Args: &taskArgs,
+		Execute:    consumerFuncExecute,
+		OnComplete: consumerFuncComplete,
+		OnError:    consumerFuncError,
+	}
+	workerpool.AddTask(&task)
+	workerpool.StartWork(ctx)
+}
+
+func (p *ProcessEndpoint) DisableBuffer() {
+
 }
 
 func (p *ProcessEndpoint) ClosePipes() error {
-	e1 := p.in.Close()
-	e2 := p.out.Close()
+	e1 := p.inPipe.Close()
+	e2 := p.outPipe.Close()
 
 	return errors.Join(e1, e2)
 }
