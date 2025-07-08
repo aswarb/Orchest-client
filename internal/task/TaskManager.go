1a4338641051d037f42b005ec31fe4cfa69013d0
aswarb <andrewoswarbrick@hotmail.co.uk>
2025-07-08 20:28:00 +0100

    Remove references to TaskEngine - not needed

--- internal/task/TaskManager.go
+++ internal/task/TaskManager.go
@@ -22,15 +22,39 @@ func MapHasKey[K comparable, V any](m map[K]V, key K) bool {
 }
 
 type TaskManager struct {
-	engine   *TaskEngine
-	resolver *DAGResolver
+	resolver  *DAGResolver
+	scheduler *TaskScheduler
+	executor  *TaskExecutor
 }
 
-func (t *TaskManager) StartTask(ctx context.Context) {
-	t.engine.ExecuteTasksInOrder(ctx)
+func (t *TaskManager) StartTask(ctx context.Context) error {
+	var err error
+	for err != nil {
+		taskId, err := t.scheduler.GetNext()
+		if err != nil {
+			return err
+		}
+		node, exists := t.resolver.GetNode(taskId)
+		if exists {
+			task := node.(*Task)
+			nextTasks := []*Task{}
+			for _, nextUid := range task.GetNext() {
+				nextNode, nextExists := t.resolver.GetNode(nextUid)
+				if !nextExists {
+					continue
+				}
+				nextTask := nextNode.(*Task)
+				nextTasks = append(nextTasks, nextTask)
+			}
+			t.executor.ExecuteTask(task, nextTasks)
+			t.scheduler.MarkDone(task.GetUid(), task.GetNext())
+		}
+	}
+	return err
 }
 
-func GetTaskManagerFromToml(sourceTasks []TomlTask, sourceSegments []TomlSegment) *TaskManager {
+// Broken until TaskManager implementation is finished again
+func GetTaskManagerFromToml(sourceTasks []TomlTask) *TaskManager {
 	tasks := []Node{}
 	for _, target := range sourceTasks {
 		switch target.(type) {
